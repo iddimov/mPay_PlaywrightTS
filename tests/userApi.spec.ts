6041aeb90c5761bda35c499d1eb7eb7f35cded49
iddimov <jd.dimov@gmail.com>
2024-07-30 13:14:11 +0100

    test name update

--- tests/userApi.spec.ts
+++ tests/userApi.spec.ts
@@ -27,7 +27,7 @@ test.describe('User API Tests', () => {
     const getUserResponse = await userApi.getUser(newUserId); 
     const getResponseBody = await getUserResponse.json();
     
-    test.step('GET - User\'s data returns 200', ()=>{      
+    test.step('GET - User\'s data return 200', ()=>{      
       expect(getUserResponse.status()).toBe(200);
     })   
     
